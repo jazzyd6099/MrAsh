e615ee82e4e04e072f1e67985fe5661cf7f65e55
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-03-02 18:26:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ const prefix = "_";
 client.on('ready', () => {
 
     console.log('Lets get this show on the road!');
+    
+        client.user.setStatus('idle', 'helping you!') 
 });
 
       client.on("message", (message) => {
