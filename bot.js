fd80640f32a3b0db8d94fa2561ab8f76a7486831
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-03-02 18:21:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,28 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+const prefix = "_";
 
- 
 
 client.on('ready', () => {
 
-    console.log('I am ready!');
+    console.log('Lets get this show on the road!');
 
+ 	client.user.setPresence({ game: { name: 'CPAVTS' }, status: 'online' })
+  .then(console.log)
+  .catch(console.error);
 });
 
- 
-
+      client.on("message", (message) => {
+    	if (message.content.startsWith("Hello mr ash!")) {
+      		message.reply("What's up?");
+     }
+  });  
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong!');
+ 
+  if (!message.content.startsWith(prefix)) return;
+ 
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
 
        }
 
