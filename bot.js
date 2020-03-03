6e537f576091c8090ab1981562ff419c51201a49
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-03-02 19:00:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,19 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "_";
 
+//embedColors
+
+const embedRed = 0xff0000
+const embedOrange = 0xff790c
+const embedYellow = 0xffff00
+const embedGreen = 0x00ff00
+const embedBlue = 0x0064ff
+const embedPurple = 0x6a00b0
+const embedMagenta = 0x9600ff
+const embedPink = 0xff00ff
+const embedBlack = 0x000000
+const embedWhite = 0xffffff
+const embedGray = 0x777777
 
 client.on('ready', () => {
 
@@ -22,15 +35,35 @@ client.on('ready', () => {
       client.on("message", (message) => {
         	if (message.content.startsWith("Hello mr ash!")) {
          		message.reply("What's up?");
+            } else
+                if (message.content == "Hello Mr. Ash")) {
+                    message.reply("What's up?");
+                } else
+                    if (message.content == "hello mr ash")) {
+                        message.reply("What's up?');
             
      }
   });  
+           client.on("message", (message) => {
+               
+               if (message.content == "Hello! Here are the commands of what I can do!")) {
+                    message.channel.send({embed: {
+	    				color: embedYellow,
+	    				title: "Mr. Ash Commands",
+	    				description: "!help that's it so far. Hope i'm making the server fun!",
+                        
+                    }
+           });
+               
 client.on('message', message => {
  
   if (!message.content.startsWith(prefix)) return;
  
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
+  } else
+      if (message.content.startsWith(prefix + "help)) {
+          message.author.send("Hello! Here are the commands of what I can do!");
 
        }
 
