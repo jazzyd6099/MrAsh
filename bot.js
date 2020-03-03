55cc8066ab27d23c405d264a2ad8e6048b7342c3
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-03-02 19:14:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,9 +38,6 @@ client.on('ready', () => {
             } else
                     if (message.content.startsWith("hello mr ash")) {
                         message.reply("What's up?");
-                    } else
-                    if (message.content == "Hello! Here are the commands of what I can do!")) {
-                    message.channel.send("Currently WIP.");
               }
   });  
                
