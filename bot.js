18ea01d08c846699932dc998eedfc7813edfc1a3
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-03-02 20:43:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,8 @@ client.on('ready', () => {
 
     console.log('Lets get this show on the road!');
     
-	client.user.setActivity("Staying cool"); 
-       client.user.setPresence({ activity: { name: 'staying cool!' }, status: 'idle' })
+	client.user.setActivity("Staying cool!"); 
+       client.user.setPresence({ activity: { name: 'staying cool' }, status: 'idle' })
   .then(console.log)
   .catch(console.error);
 });
@@ -47,7 +47,10 @@ client.on('ready', () => {
 					message.reply("No, YOU'RE cool!");
 				} else
 					if (message.content.startsWith("You're so cool")) {
-						message.reply("No, YOU'RE cool!"); 
+						message.reply("No, YOU'RE cool!");
+					} else
+						if(message.content.startsWith("You're so cool")) {
+							message.react(":sunglasses:");
               }
   });  
                
